f2e4266ba2a87bf47d390a3c2a137e4bd3551546
GIT_NAME <GIT_EMAIL>
2023-03-27 15:49:04 -0300

    add initial screen

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -480,7 +480,7 @@ return {
       require("telescope").load_extension("notify")
     end
   },
-  -- { -- quebrou a descrição dos autocomplete (modal)
+  -- { -- break the autocomplete (modal)
   --   "folke/noice.nvim",
   --   dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
   --   config = function()
@@ -528,6 +528,60 @@ return {
           },
         },
       })
+    end
+  },
+  { -- splash/initial screen
+    "glepnir/dashboard-nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require('dashboard').setup({
+        theme = 'hyper',
+        shortcut_type = 'number',
+        config = {
+          week_header = {
+            enable = true,
+          },
+          shortcut = {
+            { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
+            {
+              icon = ' ',
+              icon_hl = '@variable',
+              desc = 'Files',
+              group = 'Label',
+              action = 'Telescope find_files',
+              key = 'f',
+            },
+            {
+              desc = ' Apps',
+              group = 'DiagnosticHint',
+              action = 'Telescope app',
+              key = 'a',
+            },
+            {
+              desc = ' dotfiles',
+              group = 'Number',
+              action = 'Telescope dotfiles',
+              key = 'd',
+            },
+            {
+              desc = '✸ new',
+              group = '@property',
+              action = 'e new',
+              key = 'n',
+            },
+          }
+        },
+      })
+
+      -- TODO: try to put this in somewhere
+      -- vim.g.startup_bookmarks = {
+      --   ["V"] = '~/.config/nvim/init.lua',
+      --   ["P"] = '~/.config/nvim/lua/plugins.lua',
+      --   ["G"] = '~/.gitconfig',
+      --   ["Z"] = '~/.zshrc',
+      --   ["S"] = '~/.ssh/config',
+      -- }
     end
   }
 }
